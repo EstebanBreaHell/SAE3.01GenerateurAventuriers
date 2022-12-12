c139d996e206b371767d0cff66563f9740bd30d9
XoleKo <xolekoiut76@gmail.com>
2022-12-12 15:02:47 +0100

    Classe CarteWagon

---
+++ Projet/src/metier/CarteWagon.java
@@ -0,0 +1,17 @@
+package metier;
+
+import java.awt.*;
+
+public class CarteWagon {
+
+    private Color couleur;
+
+    public CarteWagon(Color couleur) {
+        this.couleur = couleur;
+    }
+
+    public Color getCouleur() {
+        return this.couleur;
+    }
+
+}
