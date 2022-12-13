e30ac8d9d1721ad8cf79605d8f776313ff60ab3f
EstebanBreaHell <esteban.brea-hell@etu.univ-lehavre.fr>
2022-12-13 05:49:24 +0100

    Début d'amélioration de la class Metier
    Creation des méthode pour la génération des arrete et des Noeud

--- Projet/src/metier/Metier.java
+++ Projet/src/metier/Metier.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.awt.Color;
 
 public class Metier
 {
@@ -14,11 +15,13 @@ public class Metier
     private Controleur ctrl;
 
 	private List<Noeud> lstNoeud;
+	private List<Arrete> lstArrete;
 
     public Metier( Controleur ctrl )
     {
         this.ctrl = ctrl;
 		this.lstNoeud = new ArrayList<Noeud>();
+		this.lstArrete = new ArrayList<Arrete>();
     }
 
     public void ecrireXml()
@@ -46,6 +49,45 @@ public class Metier
 		}
 
 	}
+
+	public void creeNoeud( String nom, int x, int y )
+	{
+		Noeud n = new Noeud( nom, x, y );
+		lstNoeud.add( n );
+	}
+
+	public boolean creeArrete(String noeud1 , String noeud2 , String couleur, int wagon )
+	{
+		Noeud n1 = null;
+		Noeud n2 = null;
+
+		//TODO Faut regarder pour géré la couleur (Vus que les couleur sont déterminé de base dans le jeu on peux faire une liste déroulante dans l'ihm)
+		Color c = Color.BLACK;
+		
+
+		/*
+		*Vus que le joueur nous donne juste les Nom en string on vas regarder si ils éxiste dans la liste des noeud
+		*TODO car j'ai la flemme peux être voir pour faire une liste déroulante dans l'ihm
+		*/
+		for( Noeud n : lstNoeud )
+		{
+			if( n.getNom().equals( noeud1 ) )
+				n1 = n;
+			if( n.getNom().equals( noeud2 ) )
+				n2 = n;
+		}
+
+		if( n1 != null && n2 != null )
+		{
+			Arrete a = new Arrete( n1, n2, c ,wagon);
+			lstArrete.add( a );
+
+			return true;
+		}
+		else
+			return false;
+	}
+
 
 
 
