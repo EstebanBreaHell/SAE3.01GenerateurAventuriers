fd5fa8e25fc0032166c88299fb857de525d03eb5
Adrien Decharrois <95859046+AdrienDchrs@users.noreply.github.com>
2022-12-25 19:14:50 +0100

    Liste + court trvail

--- Projet/ihm/sectioncreer/PanelParametre.java
+++ Projet/ihm/sectioncreer/PanelParametre.java
@@ -24,12 +24,14 @@ public class PanelParametre extends JPanel implements ActionListener
 	private JButton btnmoins;
 	private JButton btnplus2;
 	private JButton btnmoins2;
-	private JButton btnEditer;
+	private JButton btnImporterImg;
+	private JButton btnImporterImg2;
 	private JButton btnConfirmer;
 	private JTextField btnCompter;
 	private JTextField btnCompter2; 
 	private JTextField txtNom;
 	private JTextField txtNomMoyenDeTransport;
+	private JTextField txtFinDePartie;
 	private JDialog jd;
 
 
@@ -67,12 +69,14 @@ public class PanelParametre extends JPanel implements ActionListener
 		this.btnCompter2 			= new JTextField("1");
 		this.txtNbCouleurs			= new JTextField("1");
 		this.txtNom					= new JTextField(("route,piste cyclable,rails"));
+		this.txtFinDePartie			= new JTextField();
 		this.btnValider 			= new JButton("Générer XML");
 		this.btnplus 				= new JButton("+");
 		this.btnplus2 				= new JButton("+");
 		this.btnmoins 				= new JButton("-");
 		this.btnmoins2 				= new JButton("-");
-		this.btnEditer 				= new JButton("Importer image moyen de transport");
+		this.btnImporterImg 		= new JButton("Importer image moyen de transport");
+		this.btnImporterImg2 		= new JButton("Importer image multicolore");
 		this.btnConfirmer 			= new JButton("Confirmer");
 
 		JLabel lblNbJoueursMin 			= new JLabel("Nb joueurs minimum : ");
@@ -86,6 +90,7 @@ public class PanelParametre extends JPanel implements ActionListener
 		JLabel lblCouleur		 		= new JLabel("Nb couleurs : ");
 		JLabel lblNom 					= new JLabel("Type arête :");
 		JLabel lblNomMoyenDeTransport 	= new JLabel("Type moyen de transport :");
+		JLabel lblFinDePartie			= new JLabel("Nb " + this.txtNomMoyenDeTransport.getText() + " pour fin de partie : ");
 
 		JTable tableau = new JTable(donnees, entetes);
 
@@ -107,7 +112,8 @@ public class PanelParametre extends JPanel implements ActionListener
 		this.btnCompter.setHorizontalAlignment(SwingConstants.CENTER);
 		this.btnCompter2.setHorizontalAlignment(SwingConstants.CENTER);
 		this.btnValider.setBackground(Color.WHITE);
-		this.btnEditer.setBackground(Color.WHITE);
+		this.btnImporterImg.setBackground(Color.WHITE);
+		this.btnImporterImg2.setBackground(Color.WHITE);
 		this.txtNom.setForeground(Color.GRAY);
 		this.txtNomMoyenDeTransport.setForeground(Color.GRAY);
 
@@ -121,19 +127,22 @@ public class PanelParametre extends JPanel implements ActionListener
 		this.btnCompter2.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 		
 
-		this.btnplus	.setBounds( 50,  30,  60,  20);
-		this.btnCompter .setBounds( 50,  50,  60,  60);
-		this.btnmoins	.setBounds( 50, 110,  60,  20);
-		this.btnEditer	.setBounds(330,  85, 245,  25);
-		this.btnplus2	.setBounds( 50,  200, 60,  20);
-		this.btnCompter2.setBounds( 50,  220, 60,  60);
-		this.btnmoins2	.setBounds( 50,  280, 60,  20);
-		lblCarteMulti	.setBounds( 50,  220,250,  60);
-		lblPoints		.setBounds(  0,  240,250, 250);
-		lblNbWagonsMulti.setBounds(  0,   40,250, 250);
-		lblCarteWagon	.setBounds(100,   50,250,  60);
-		panelTableau	.setBounds( 50,  390,400, 117);
-		jd				.setBounds(900,  300,500, 400); 	
+		this.btnplus		.setBounds( 50,  30,  60,  20);
+		this.btnCompter 	.setBounds( 50,  50,  60,  60);
+		this.btnmoins		.setBounds( 50, 110,  60,  20);
+		this.btnImporterImg	.setBounds(330,  85, 245,  25);
+		this.btnImporterImg2.setBounds(230, 255, 200,  25);
+		this.btnplus2		.setBounds( 50,  200, 60,  20);
+		this.btnCompter2	.setBounds( 50,  220, 60,  60);
+		this.btnmoins2		.setBounds( 50,  280, 60,  20);
+		lblCarteMulti		.setBounds( 50,  220,250,  60);
+		lblPoints			.setBounds(  0,  270,250, 250);
+		lblNbWagonsMulti	.setBounds(  0,   40,250, 250);
+		lblCarteWagon		.setBounds(100,   50,250,  60);
+		panelTableau		.setBounds( 50,  420,400, 117);
+		lblFinDePartie		.setBounds(  0,  215,250, 250);
+		this.txtFinDePartie .setBounds(250,  330,100,  20);
+		jd					.setBounds(900,  300,500, 400); 	
 		this.btnCompter2.setEditable(false);
 		this.btnCompter	.setEditable(false);
 
@@ -182,17 +191,21 @@ public class PanelParametre extends JPanel implements ActionListener
 		panelChoixCarte.add(this.btnCompter);
 		panelChoixCarte.add(this.btnmoins);
 		panelChoixCarte.add(lblCarteWagon);
-		panelChoixCarte.add(this.btnEditer);
+		panelChoixCarte.add(this.btnImporterImg);
 
 		panelChoixCarte.add(lblNbWagonsMulti);
+		panelChoixCarte.add(this.btnImporterImg2);
 
 		panelChoixCarte.add(this.btnplus2);
 		panelChoixCarte.add(this.btnCompter2);
 		panelChoixCarte.add(this.btnmoins2);
 		panelChoixCarte.add(lblCarteMulti);
+
+
 		panelChoixCarte.add(lblPoints);
 		panelChoixCarte.add(panelTableau);
-
+		panelChoixCarte.add(lblFinDePartie);
+		panelChoixCarte.add(this.txtFinDePartie);
 		panelTableau.add(tableau.getTableHeader(), BorderLayout.NORTH);
 		panelTableau.add(tableau, BorderLayout.CENTER);
 
@@ -213,7 +226,8 @@ public class PanelParametre extends JPanel implements ActionListener
 		this.btnplus2.addActionListener(this);
 		this.btnmoins2.addActionListener(this);
 		this.btnValider.addActionListener(this);
-		this.btnEditer.addActionListener(this);
+		this.btnImporterImg.addActionListener(this);
+		this.btnImporterImg2.addActionListener(this);
 		this.btnConfirmer.addActionListener(this);
 
 		/**
@@ -341,7 +355,7 @@ public class PanelParametre extends JPanel implements ActionListener
 
 			if(this.txtNbCartesJoueurs.getText().isEmpty() || this.txtNbJoueursMax.getText().isEmpty() || 
 			   this.txtNbJoueursMin.getText().isEmpty()    || this.txtNom.getText().isEmpty()		   || 
-			   this.txtNomMoyenDeTransport.getText().isEmpty() || this.txtNbCouleurs.getText().isEmpty())
+			   this.txtNomMoyenDeTransport.getText().isEmpty() || this.txtNbCouleurs.getText().isEmpty() || this.txtFinDePartie.getText().isEmpty())
 			{
 				JOptionPane.showMessageDialog(this, "Veuillez remplir tous les champs", "Erreur", JOptionPane.ERROR_MESSAGE);
 			}
@@ -352,9 +366,10 @@ public class PanelParametre extends JPanel implements ActionListener
 				int nbCartesJoueurs = Integer.parseInt(this.txtNbCartesJoueurs.getText());
 				int nbWagonsMax = Integer.parseInt(this.btnCompter.getText());
 				int nbWagonsMulti = Integer.parseInt(this.btnCompter2.getText());
+				int nbFinDePartie = Integer.parseInt(this.txtFinDePartie.getText());
 
 				if(nbJoueursMin > nbJoueursMax || nbJoueursMin <= 0 || nbJoueursMax <= 0 || nbCartesJoueurs <= 0 || nbWagonsMax <= 0  || 
-				nbWagonsMulti <= 0)
+				nbWagonsMulti <= 0 || nbFinDePartie < 0)
 				{
 					JOptionPane.showMessageDialog(this, "Veuillez remplir tous les champs", "Erreur", JOptionPane.ERROR_MESSAGE);
 				} 
@@ -377,7 +392,27 @@ public class PanelParametre extends JPanel implements ActionListener
 			jd.dispose();
 		}
 		
-		if(e.getSource() == this.btnEditer)
+		if(e.getSource() == this.btnImporterImg)
+		{
+			FileNameExtensionFilter filtre = new FileNameExtensionFilter("format image(*.png; *.jpg; *.gif)", "png","jpg","gif");
+			JFileChooser jFileChooser = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+
+			jFileChooser.setAcceptAllFileFilterUsed(false);
+			jFileChooser.setFileFilter(filtre);
+
+			int res = jFileChooser.showOpenDialog(null);
+			if(res == JFileChooser.APPROVE_OPTION)
+			{
+				File file = jFileChooser.getSelectedFile();
+				
+				try 	{Files.copy(file.toPath(), Paths.get("importe\\"+file.getName()));} 
+				catch (IOException e1) {e1.printStackTrace();}
+			}
+
+			this.ctrl.majPanelImporter();
+		}
+
+		if(e.getSource() == this.btnImporterImg2)
 		{
 			FileNameExtensionFilter filtre = new FileNameExtensionFilter("format image(*.png; *.jpg; *.gif)", "png","jpg","gif");
 			JFileChooser jFileChooser = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
