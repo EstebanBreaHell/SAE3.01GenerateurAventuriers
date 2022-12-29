2accb27f73660e1bf77fba3a4e532c75a8361d16
Gamecraft400 <102668085+Gamecraft400@users.noreply.github.com>
2022-12-30 00:14:05 +0100

    Petit correction pour panelApercu

--- Projet/main/Controleur.java
+++ Projet/main/Controleur.java
@@ -181,6 +181,17 @@ public class Controleur
         this.metier.supprNoeud(noeud);
     }
 
+    public BufferedImage createImage(JPanel panel) {
+
+        int w = panel.getWidth();
+        int h = panel.getHeight();
+        BufferedImage bi = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g = bi.createGraphics();
+        panel.paint(g);
+        g.dispose();
+        return bi;
+    }
+
     public void supprArete(int arete)
     {
         this.metier.supprArete(arete);
